ea4a7b598b4d719eb2fc78491e5a03dd7f3d672f
Bruno Brocanelli <bbrocanelli@gmail.com>
2022-04-30 20:15:34 -0300

    Adicionando Projeto

---
+++ main.py
@@ -0,0 +1,96 @@
+from turtle import up
+import pygame
+import sys
+
+#General Setup
+pygame.init()
+clock = pygame.time.Clock()
+
+FPS = 60
+WIDTH, HEIGHT = 700, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong!")
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 20,100
+
+#COLORS
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+
+class Paddle:
+    COLOR = WHITE
+    VEL = 4
+
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win,self.COLOR, (self.x,self.y,self.width,self.height))
+
+    
+    def move(self, up = True):
+        if up:
+            self.y -= self.VEL
+
+        else:
+            self.y += self.VEL    
+
+
+
+
+def draw_window(win, paddles):
+    win.fill(BLACK)
+
+    for paddle in paddles:
+         paddle.draw(win)
+    
+    pygame.display.update()
+
+
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w]:
+        left_paddle.move(up=True)
+    
+    if keys[pygame.K_s]:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP]:
+        right_paddle.move(up=True)
+    
+    if keys[pygame.K_DOWN]:
+        right_paddle.move(up=False)
+
+     
+
+
+def main():
+    run = True
+
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH,PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH,PADDLE_HEIGHT)
+
+
+    while run:
+        draw_window(WIN,[left_paddle,right_paddle])
+        clock.tick(FPS)
+        for event in pygame.event.get():
+              if event.type == pygame.QUIT:
+                  pygame.quit()
+                  sys.exit()
+        
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
+                
+
+
+
+
+if __name__ == "__main__":
+    main()
+
