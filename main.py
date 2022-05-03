50052081013f8d9dea909c1adab7b0d78bd49471
Bruno Brocanelli <bbrocanelli@gmail.com>
2022-05-03 09:58:49 -0300

    Adicionando Pontuação

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from multiprocessing import reduction
 from re import X
-from turtle import up
+from turtle import left, up
 import pygame
 import sys
 
@@ -22,6 +22,8 @@ BLACK = (0,0,0)
 RED = (255,0,0)
 
 BALL_RADIUS = 7
+
+SCORE_FONT = pygame.font.SysFont("comicsans", 50)
 
 class Paddle:
     COLOR = WHITE
@@ -50,8 +52,8 @@ class BALL:
     COLOR = RED
 
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
+        self.x = self.original_x = x
+        self.y = self.original_y = y
         self.radius = radius
         self.x_vel = self.MAX_VEL
         self.y_vel = 0
@@ -63,10 +65,22 @@ class BALL:
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
+    
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.y_vel = 0
+        self.x_vel *= -1
+        
 
 
-def draw_window(win, paddles, ball):
+def draw_window(win, paddles, ball, left_score, right_score):
     win.fill(BLACK)
+
+    left_score_text = SCORE_FONT.render(f"{left_score}", 1, WHITE)
+    right_score_text = SCORE_FONT.render(f"{right_score}", 1, WHITE)
+    win.blit(left_score_text, (WIDTH//4 - left_score_text.get_width()//2, 20))
+    win.blit(right_score_text, (WIDTH * (3/4) - right_score_text.get_width()//2, 20))
 
     for paddle in paddles:
          paddle.draw(win)
@@ -132,8 +146,11 @@ def main():
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH,PADDLE_HEIGHT)
     ball = BALL(WIDTH//2, HEIGHT//2, BALL_RADIUS)
 
+    left_score = 0
+    right_score = 0
+
     while run:
-        draw_window(WIN,[left_paddle,right_paddle], ball)
+        draw_window(WIN,[left_paddle,right_paddle], ball, left_score, right_score)
         clock.tick(FPS)
         for event in pygame.event.get():
               if event.type == pygame.QUIT:
@@ -146,6 +163,13 @@ def main():
 
         ball.move()
         handle_collision(ball, left_paddle, right_paddle)
+
+        if ball.x < 0:
+            right_score += 1
+            ball.reset()
+        elif ball.x > WIDTH:
+            left_score += 1
+            ball.reset() 
                 
 
 
